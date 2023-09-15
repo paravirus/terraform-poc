6bb4b67cedf4758b9652cc40ef172c3f16b54610
ParaV!ru$ <60077714+paravirus@users.noreply.github.com>
2023-09-15 12:24:46 -0400

    Update ec2.tf

--- terraform_modules/ec2.tf
+++ terraform_modules/ec2.tf
@@ -1,9 +1,9 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "{ec2-name}"
+  name = {ec2-name}
 
-  instance_type          = "{instance-type}"
+  instance_type          = {instance-type}
   key_name               = "user1"
   monitoring             = true
   vpc_security_group_ids = ["sg-12345678"]
