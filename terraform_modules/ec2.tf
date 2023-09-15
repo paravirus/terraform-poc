82c6f9b1a41b49872c3faed722d534ef39debbde
ParaV!ru$ <60077714+paravirus@users.noreply.github.com>
2023-09-15 00:54:11 -0400

    Create ec2.tf

---
+++ terraform_modules/ec2.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "{ec2-name}"
+
+  instance_type          = "{instance-type}"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    {key}   = "{value}"
+    Environment = "dev"
+  }
+}
