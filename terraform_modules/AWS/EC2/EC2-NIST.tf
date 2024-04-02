97479d2a47d1832bd2eb347605b78bbbc56e0aed
ParaV!ru$ <60077714+paravirus@users.noreply.github.com>
2024-04-02 00:41:37 -0400

    Create EC2-NIST.tf

---
+++ terraform_modules/AWS/EC2/EC2-NIST.tf
@@ -0,0 +1,16 @@
+module "ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  NIST
+  name = {ec2-name}
+
+  instance_type          = {instance-type}
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    {key}   = "{value}"
+    Environment = "dev"
+  }
+}
